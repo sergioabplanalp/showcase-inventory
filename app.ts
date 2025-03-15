37d28df9f08e925db186eda5f67d6cbbc2f4699b
sergioabplanalp <hello@sergioabplanalp.com>
2025-03-15 21:15:55 +0100

    Code cleanup

--- app.ts
+++ app.ts
@@ -4,10 +4,6 @@ import * as productGenerator from "./src/service/generator";
 const app = express();
 app.use(express.json());
 
-app.get('/inventory', (req, res) => {
-  res.send('hello inventory.');
-});
-
 app.listen(8080, () => {
   console.log('Server running on port 8080');
 });
